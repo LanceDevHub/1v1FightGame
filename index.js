a60923c5655729a3429bd0135a8fbd9f22955d69
Opticpeak <opticpeak@gmail.com>
2024-04-04 18:59:40 +0200

    added punching animatiion for player 1.

--- index.js
+++ index.js
@@ -15,21 +15,72 @@ const gravity = 0.2
 
 // sprite class
 class Sprite {
-    constructor({position, velocity, size}) {
+    constructor({position, velocity, size, healthBar}) {
         this.position = position
         this.velocity = velocity
         this.size = size
+        this.healthBar = healthBar
+        this.attackFinished = false
     }
 
+
     // draw sprite
-    draw() {
+    drawSprite() {
         c.fillStyle = "red"
         c.fillRect(this.position.x, this.position.y, this.size.width, this.size.height)
+    }
+    // draw healthBar
+    drawHealthBar() {
+        c.fillStyle = "green"
+        // player 1 healthBar
+        if (this.healthBar.player === 1) {
+            if (this.healthBar.health <= 0) {
+                c.fillRect(30, 20, 0, 20)
+            } else {
+                c.fillRect(30, 20, 150 / 100 * this.healthBar.health, 20)
+            }
+        // player 2 healthBar
+        } else if (this.healthBar.player === 2) {
+            if (this.healthBar.health <= 0) {
+                c.fillRect(1024 - this.size.height , 20, 0, 20)
+            } else {
+                c.fillRect(1024 - this.size.height - 30, 20, 150 / 100 * this.healthBar.health, 20)
+            }
+        }
+    }
+
+    drawHand() {
+        c.fillStyle = "orange"
+        c.fillRect(this.position.x, this.position.y + 50, 75, 25)
+    }
+
+
+    // TODO:
+        // let the doAttack be a class variable instead of a global one
+        // attackAnimation and attackRate timer as well to make it individual for every sprite instance
+    drawAttack() {
+        c.fillStyle = "orange"
+        c.fillRect(this.position.x, this.position.y + 50, 75 + 25 / 20 * attackAnimationTimer, 25)
+        if (attackAnimationTimer >= 20) {
+            this.attackFinished = true
+        } else if (attackAnimationTimer <= 0) {
+            this.attackFinished = false
+            doAttack = false
+        }
+        if (this.attackFinished === false) {
+            attackAnimationTimer += 1
+        } else if (this.attackFinished === true) {
+            attackAnimationTimer -= 1
+        }
+
     }
 
     // update sprite e.g related to gravity etc.
     update() {
-        this.draw()
+        this.drawSprite()
+        this.drawHealthBar()
+        this.drawHand()
+
 
         // update position
         this.position.y += this.velocity.y
@@ -68,9 +119,13 @@ const player = new Sprite({
         x: 0,
         y: 0
     },
-    size : {
+    size: {
         width: 50,
         height: 150
+    },
+    healthBar: {
+        health: 100,
+        player: 1
     }
 })
 
@@ -84,9 +139,13 @@ const enemy = new Sprite({
         x: 0,
         y: 0
     },
-    size : {
+    size: {
         width: 50,
         height: 150
+    },
+    healthBar: {
+        health: 100,
+        player: 2
     }
 })
 
@@ -100,11 +159,17 @@ const animate = () => {
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
-
+    if (doAttack) {
+        player.drawAttack()
+    }
+    player.healthBar.health -= 0.75
+    enemy.healthBar.health -= 0.5
     doubleJumpTimer += 1
+    attackRateTimer += 1
 
     // MOVEMENT-PART in animation loop
-        // left right movement
+
+        // left/right-movement
     if (keys.a.pressed && lastKey === "a") {
         player.velocity.x = -4
     } else if (keys.d.pressed && lastKey === "d") {
@@ -112,18 +177,27 @@ const animate = () => {
     } else {
         player.velocity.x = 0
     }
-        // jump movement
+        // jump-movement
             // initial jump
     if (keys.w.pressed && player.position.y + player.size.height === canvas.height){
         player.velocity.y = -5
         doubleJumpTimer = 0
         jumpCount += 1
     }
-            // double jump
+            // double-jump
     if (keys.w.pressed && (player.position.y + player.size.height) !== canvas.height && jumpCount === 1 && doubleJumpTimer >= 20) {
         player.velocity.y = -5
         jumpCount = 0
     }
+
+    // ATTACKING
+    if (keys.p.pressed && attackRateTimer >= 60) {
+        doAttack = true
+        attackRateTimer = 0
+    }
+
+
+    // delete later on just for checking some insights
     console.log(doubleJumpTimer)
 
 }
@@ -141,13 +215,23 @@ const keys = {
     },
     w: {
         pressed: false
+    },
+    p:  {
+        pressed: false
     }
 }
+
     // includes last pressed key
 let lastKey = undefined
     // essential for double jump
 let jumpCount = 0
 let doubleJumpTimer = 0
+    // essential for attacking
+        // attack rate timer
+let attackRateTimer = 0
+        // attack animation timer
+let attackAnimationTimer = 1
+let doAttack = false
 
 
 
@@ -165,6 +249,9 @@ window.addEventListener("keydown", (event) => {
         case "w":
             keys.w.pressed = true
             break
+        case "p":
+            keys.p.pressed = true
+            break
     }
     console.log(event.key)
 })
@@ -180,6 +267,9 @@ window.addEventListener("keyup", (event) => {
             break
         case "w":
             keys.w.pressed = false
+            break
+        case "p":
+            keys.p.pressed = false
             break
     }
     console.log(event.key)
